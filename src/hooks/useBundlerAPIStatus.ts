3436226dbe8edd529311e743aec25494f6e36713
Nawias <bronybracket@gmail.com>
2025-11-10 20:37:47 +0100

    * Fixed maintenance mode switch + Added  useBundlerAPIStatus hook + Added maintenance mode popup when API is offline + Pretty spinner :)

---
+++ src/hooks/useBundlerAPIStatus.ts
@@ -0,0 +1,30 @@
+import { API_URL } from "@/config";
+import { useEffect, useState } from "react";
+
+const healtCheckURL = API_URL + "/health";
+
+export default function useBundlerAPIStatus() {
+  const [online, setOnline] = useState(true);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    let cancelled = false;
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(healtCheckURL);
+        if (!res.ok) {
+          throw new Error("HTTP status code: " + res.status);
+        } else if (!cancelled) setOnline(true);
+      } catch (error) {
+        console.error(error);
+        if (!cancelled) setOnline(false);
+      }
+      setLoading(false);
+    };
+    fetchData();
+    return () => {
+      cancelled = true;
+    };
+  }, []);
+  return { online, loading };
+}
