ca6f01b9f4f4e79343be9ba18b4a41fbfdc0829b
TurtleP <jpostelnek@outlook.com>
2025-12-24 19:45:07 -0500

    maintenance: clean up

--- src/services/maintenance.ts
+++ src/services/maintenance.ts
@@ -1,8 +1,6 @@
 import { Injectable, signal } from '@angular/core';
 import { ApiService } from '@/services/api';
 
-const DEFAULT_INTERVAL = 5 * 60 * 1000;
-
 @Injectable({
   providedIn: 'root',
 })
@@ -13,17 +11,15 @@ export class MaintenanceService {
     void this.init();
   }
 
-  async checkHealth(): Promise<void> {
-    await this.service.health().catch(() => this.enabled.set(true));
-  }
-
   async init(): Promise<void> {
-    this.enabled.set(await this.service.maintenance());
-    await this.checkHealth();
-    setInterval(async () => {
-      await this.checkHealth();
-    }, DEFAULT_INTERVAL);
-    console.log(this.enabled());
+    try {
+      if (await this.service.maintenance()) {
+        this.enabled.set(true);
+      }
+      await this.service.health();
+    } catch (error) {
+      this.enabled.set(true);
+    }
   }
 
   isEnabled(): boolean {
